f197b38cfa39fc8fb69b09514ec5982b239cff1d
CODE NOVA <codenova31@gmail.com>
2025-12-31 18:02:06 +0530

    backend hosted successful

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -15,6 +15,8 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 app.use('/api', registerRoutes);
-
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
